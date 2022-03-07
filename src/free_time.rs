c570c9cb8392eb994698600f91afaedd9e269bed
gabrielseibel1 <gabrielseibel1@gmail.com>
2022-03-07 16:40:12 -0300

    Make free_time route returnw form data as json

--- src/free_time.rs
+++ src/free_time.rs
@@ -1,5 +1,40 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder};
 
-pub async fn registrer_free_time() -> impl Responder {
-    HttpResponse::Ok().body("Received.")
+use serde::{Deserialize, Serialize};
+
+use chrono::{DateTime, Utc, TimeZone};
+
+#[derive(Deserialize)]
+pub struct FreeTimeForm {
+    name: String,
+    phone: String,
+    email: String,
+    date: String,
+    time: String,
+    comment: String,
+}
+
+#[derive(Serialize)]
+pub struct FreeTime {
+    customer: String,
+    phone: String,
+    email: String,
+    datetime: DateTime<Utc>,
+    comment: String,
+}
+
+pub async fn registrer_free_time(form: web::Form<FreeTimeForm>) -> impl Responder {
+    let ft = FreeTime{
+        customer: form.name.clone(),
+        phone: form.phone.clone(),
+        email: form.email.clone(),
+        datetime: Utc.datetime_from_str(
+            &format!("{} {}:00", form.date.clone(), form.time.clone()), "%F %T"
+        ).expect(
+            &format!("date: {}, time: {}.", form.date, form.time) //use HTTP error instead 
+        ),
+        comment: form.comment.clone(),
+    };
+    
+    HttpResponse::Ok().json(ft) // <- send response
 }
