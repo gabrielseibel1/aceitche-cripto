451afa5660d57f38d66815431993dc91bdac9a3a
Gabriel de Souza Seibel <gabrieseibel1@gmail.com>
2022-05-05 10:29:38 -0300

    restructure dirs

--- cluster/0_config_envs.sh
+++
@@ -1,7 +0,0 @@
-export EKS_CLUSTER_NAME=aceitchecripto-cluster
-export EKS_NODEGROUP_NAME=aceitchecripto-nodegroup
-export EKS_NODEGROUP_TYPE=t2.small # 2GB RAM
-export EKS_NODEGROUP_COUNT=2
-export EKS_REGION=sa-east-1
-export EKS_ACCOUNT_ID=066514095720
-export EKS_CR_ID=602401143452 # https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
--- src/version.rs
+++
@@ -1,5 +0,0 @@
-use actix_web::{HttpResponse, Responder};
-
-pub async fn get() -> impl Responder {
-    HttpResponse::Ok().body("v0.3.3")
-}
